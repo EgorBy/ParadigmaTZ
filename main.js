7cfbf87bf9cbb924d5a494e60c65e24a7befe2d0
Egor <pixer.ru@mail.ru>
2023-02-24 14:24:43 +0300

    main section was started

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  $('.top-header__watch-on-map').hover(function() {
+  $('.header-top__watch-on-map').hover(function() {
     $('.map-modal').addClass('active');
 
-    // Создание iframe с Google Maps
-    var map = $('<iframe>')
-      .attr('width', '500')
-      .attr('height', '400')
-      .attr('frameborder', '0')
-      .attr('style', 'border:0')
-      .attr('src', 'https://yandex.ru/map-widget/v1/?um=constructor%3A9d2c92408ad3e485766970307c39bb1ed1549db83eaf662a880946db0e80969b&amp;source=constructor');
-
+    // Создание iframe с Yandex Maps
+    var map = $('<iframe>').attr('width', '500')
+                       .attr('height', '400')
+                       .attr('frameborder', '0')
+                       .attr('style', 'border:0')
+                       .attr('src', 'https://yandex.ru/map-widget/v1/?um=constructor%3A9d2c92' +
+                               '408ad3e485766970307c39bb1ed1549db83eaf662a880946db0e80969b&amp;s' +
+                               'ource=constructor');
     // Вставка iframe в контейнер карты
     $('.map-modal').html(map);
   }, function() {
