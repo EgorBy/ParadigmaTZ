1e01e034accc693c53b6b6921399f780c6291c1d
Egor <pixer.ru@mail.ru>
2023-02-26 14:52:44 +0300

    popup ready

--- main.js
+++ main.js
@@ -18,19 +18,42 @@ $(document).ready(function() {
     $('.map-modal').removeClass('active');
   });
 
-  $('.header-top__burder').on('click', function() {
+
+    // при изменении состояния чекбокса, устанавливаем значение атрибута disabled для кнопки отправки формы
+  $('#agreement').change(function() {
+    if($(this).is(':checked')) {
+      $('button[type="submit"]').prop('disabled', false);
+      $(this).parent().addClass('checked');
+    } else {
+      $('button[type="submit"]').prop('disabled', true);
+      $(this).parent().removeClass('checked');
+    }
+  });
+
+  $('.header-top__contacts-call-request').on('click', function() {
+    $('.popup').addClass('open');
+  });
+
+  $('.popup__close').on('click', () => {
+    $('.popup').removeClass('open');
+  });
+
+
+  $('.header-top__burger').on('click', function() {
     $('.page__wrapper').toggleClass('open-burger');
     $('.header-nav').toggleClass('desktop-only');
-  
-    if ( $('.page__wrapper').hasClass('open-burger')) {
-      $('.header-top__burder img').attr('src', 'img/burger-black.svg');
+    $('html, body').scrollTop(0);
+
+    if (!$('.page__wrapper').hasClass('open-burger')) {
+      $('.header-top__burger img').attr('src', 'img/burger.svg');
+      $('.header-fixed').css('background-color', 'transparent');
     } else {
-      $('.header-top__burder img').attr('src', 'img/burger.svg');
+      $('.header-top__burger img').attr('src', 'img/burger-black.svg');
     }
   });
   
   if ($(window).width() <= 425) {
-    $('.header').addClass('header-fixed');
+      $('.header-top').addClass('header-fixed');
   }
 
   $(window).on('scroll', function() {
@@ -41,16 +64,17 @@ $(document).ready(function() {
         $('.header-fixed').css('background-color', 'white');
         $('.header-top__contacts-work-hours, .header-top__contacts-telephone-for-call,' +
         '.header-top__contacts-call-request').css('color', 'black');
-        $('.header-top__burder img').attr('src', 'img/burger-black.svg');
+        $('.header-top__burger img').attr('src', 'img/burger-black.svg');
       } else {
-        
         $('.header-top__contacts-work-hours, .header-top__contacts-telephone-for-call,' +
          '.header-top__contacts-call-request').css('color', 'white');
         $('.header-fixed').css('background-color', 'transparent');
         if ( !$('.page__wrapper').hasClass('open-burger')) {
-          $('.header-top__burder img').attr('src', 'img/burger.svg');
+          $('.header-top__burger img').attr('src', 'img/burger.svg');
         }
       }
     }
   });
 });
+
+
